69888389bff1f1bf0f9bcdc4a78447d0724361d2
Q <corrie.ma@protonmail.com>
2024-04-04 14:58:29 +0200

    relate blogs to users(temp)

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,21 +1,31 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable prefer-destructuring */
 const blogRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('author', { username: 1, name: 1, id: 1 });
   response.json(blogs);
 });
 
 blogRouter.post('/', async (request, response) => {
+  // related to user(temporary implement)
+  const users = await User.find({});
+  // const usersJSON = await users.map((user) => user.toJSON());
+  const user = users[0];
+
   const newBlog = Blog({
     title: request.body.title,
-    author: request.body.author,
+    author: user._id,
     url: request.body.url,
     likes: request.body.likes || 0,
   });
 
   const result = await newBlog.save();
+  user.blogs = user.blogs.concat(newBlog._id);
+  await user.save();
+
   response.status(201).json(result);
 });
 
--- models/blog.js
+++ models/blog.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
+  // author: { type: String, required: true },
   url: { type: String, required: true },
   likes: { type: Number, default: 0 },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 });
 
 blogSchema.set('toJSON', {
